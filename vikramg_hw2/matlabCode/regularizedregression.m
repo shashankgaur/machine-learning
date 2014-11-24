6601bc81181245362167147aa32fab6d7042c9ba
shashankgaur <shashankgaur@ieee.org>
2014-11-24 23:44:18 +0000

    Commit before submission on Monday nigt

--- vikramg_hw2/matlabCode/regularizedregression.m
+++
@@ -1,9 +0,0 @@
-function [weights] = regularizedregression(train_data_X, train_data_Y, c, lambda)
-
-%% TODO
-
-%2 or 3 lines of code
-Cd = diag(c,0);
-dim = size(train_data_X,2);
-weights = inv(train_data_X'*Cd*train_data_X + lambda*eye(dim))*train_data_X'*Cd*train_data_Y;
-return;
